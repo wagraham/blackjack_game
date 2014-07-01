c017adf1c4b5f4722fc18b6916d967978de976f3
wagraham <wagraham87@gmail.com>
2014-07-01 09:24:33 -0400

    some minor fixing

--- blackjack.rb
+++ blackjack.rb
@@ -2,25 +2,23 @@
 
 # I. Pseudo-Code
 
-# A. I need to create a deck of cards that contains both cases
-# and values --- The Queen  must be valued at 10, for example. 
+  # A. I need to create a deck of cards that contains both cases
+  # and values --- The Queen  must be valued at 10, for example. 
 
-# B. I must somehow distribute two cards to each player, 
-# a human and a computer
+  # B. I must somehow distribute two cards to each player, 
+  # a human and a computer
 
-# C. the program must calculate the sum of the values, and check for 
-# 21, bust, or option to hit and stay. 
+  # C. the program must calculate the sum of the values, and check for 
+  # 21, bust, or option to hit and stay. 
 
-# D. the player & dealer must take turns, and either hit or stay. 
+  # D. the player must go first; then the dealer must play. 
 
-# E. the dealer must hit unless he has at least 17.
+  # E. the dealer must hit unless he has at least 17.
 
-# F. define a winner: 21, or if one person busts (< 21), 
-# or if both stay --- the person with the closest to 21 wins. 
+  # F. define a winner: 21, or if one person busts (< 21), 
+  # or if both stay --- the person with the closest to 21 wins. 
 
-# G. print the winner and score. 
-
-
+  # G. print the winner and score. 
 
 # II. Replacing Logic with Ruby
 
@@ -58,7 +56,7 @@ cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 deck = suits.product(cards)
 deck.shuffle!
 
-# Deak Cards
+# Deal Cards
 
 mycards = []
 dealercards = []
@@ -73,19 +71,16 @@ mytotal = calculate_total(mycards)
 
 # Show Cards - I should probably begin a while loop here
 
-
-
-
 puts "Dealer has: #{dealercards[0]} and #{dealercards[1]}, for a total of #{dealertotal}"
 puts "You have: #{mycards[0]} and #{mycards[1]}, for a total of #{mytotal}"
 puts "" #to give space
 
-  # I suspect I need to loop the hit_or_stay method until someone wins
+#I suspect I need to loop the hit_or_stay method until someone wins
 
 #my turn
 
 if mytotal == 21
-  puts "Blackjack!"
+  puts "Blackjack! You win."
   exit
 end
 
@@ -99,17 +94,17 @@ while mytotal < 21
   end
 
   if hit_or_stay == "2"
-    puts "You choose to stay."
+    puts "You chose to stay."
     break
   end
   
-  #hit < no if condition to simplify things. 
+  #hit < no if condition to simplify things. If <21 evaluates to true and the prior two if conditions
+  # evaluate to false, then this code runs.
   new_card = deck.pop
   puts "Dealing card to player: #{new_card}"
   mycards << new_card
   mytotal = calculate_total(mycards)
   puts "Your total is now #{mytotal}."
-  
 
   if mytotal == 21
     puts "Blackjack!"
@@ -118,30 +113,29 @@ while mytotal < 21
     puts "Sorry, you busted."
     exit
   end
-
 end
 
 # dealer turn
 
 if dealertotal == 21
-  puts "Dealer hit Blackjack."
+  puts "Dealer hit Blackjack. A loser is you!"
   exit
 end
 
 while dealertotal < 17 
   new_card = deck.pop # hit
-  puts "Dealer hits: #{new_card}" # local variable within method score can be reused
+  puts "Dealer hits: #{new_card}" # local variable within method scope can be reused
   dealercards << new_card
-  dealertoral = calculate_total(dealercards)
-  puts "Dealer total is now: #{dealertoral}"
+  dealertotal = calculate_total(dealercards)
+  puts "Dealer total is now: #{dealertotal}"
 
-  if dealertoral == 21
+  if dealertotal == 21
     Puts "Dealer hit Blackjack."
 
-  elsif dealertoral > 21
+  elsif dealertotal > 21
     puts "Congrats. Dealer Busted. A winner is you"
+  end
 end
-
 # compare hands
 
 puts "Dealer's cards: "
@@ -156,9 +150,9 @@ end
 
 puts ""
 
-if dealertoral > mytotal
+if dealertotal > mytotal #except what if the dealer busted? This code is nicht güt. 
   puts "Sorry, dealer won"
-elsif dealertoral < mytotal
+elsif dealertotal < mytotal
   puts "You win!"
 else 
   puts "It's a tie."
